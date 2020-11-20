37f79e8f6424d01cf9a9f428250eb4e2eef1ed6d
PerkGuacamole <evanunmann@gmail.com>
2020-11-20 08:45:09 -0500

    Changing run parameters for benchmark"

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ current_time=$(date "+%Y.%m.%d.%H.%M.%S")
 file_ext=.txt
 final_name=$file_name.$current_time$file_ext
 
-alloc_size=4
+alloc_size=20
 
-echo Starting main
+echo Starting graph_analysis
 echo Allocation Size: $alloc_size GB
 echo Output File: $final_name
 
