7d0e1a15ae4da1839c92c49bb308ffe6cd3ae242
PerkGuacamole <evanunmann@gmail.com>
2021-04-07 09:33:45 -0400

    Prepping Page Rank compiler

--- include/Benchmarks.hpp
+++ include/Benchmarks.hpp
@@ -14,110 +14,110 @@
 
 namespace Benchmark {
 
-	typedef struct Parameters {
-		size_t mem_alloc_size = 1000000;
-		uint32_t graph_num_vertices = 100000;
-		uint32_t graph_min_degree = 1;
-		uint32_t graph_max_degree = 200;
-		float graph_min_value = 1;
-		float graph_max_value = 5;
-		uint32_t page_rank_iterations = 100;
-		uint32_t page_rank_num_dampening_factors = 4;
-		std::vector<size_t> page_rank_dampening_factors = { 1,2,4,8,16 };
-		uint32_t test_iterations = 10;
-		std::string graph_path = "";
-		std::string graph_name = "";
-		std::string out_dir = "";
-	} Parameters;
+    typedef struct Parameters {
+        size_t mem_alloc_size = 1000000;
+        uint32_t graph_num_vertices = 100000;
+        uint32_t graph_min_degree = 1;
+        uint32_t graph_max_degree = 200;
+        float graph_min_value = 1;
+        float graph_max_value = 5;
+        uint32_t page_rank_iterations = 100;
+        uint32_t page_rank_num_dampening_factors = 4;
+        std::vector<size_t> page_rank_dampening_factors = { 1,2,4,8,16 };
+        uint32_t test_iterations = 10;
+        std::string graph_path = "";
+        std::string graph_name = "";
+        std::string out_dir = "";
+    } Parameters;
 
-	Benchmark::Parameters get_parameters();
+    Benchmark::Parameters get_parameters();
 
-	void benchmark_page_rank(const Benchmark::Parameters& tp, const GraphCRS<std::allocator>& graph_dram, const GraphCRS<PMEM::allocator>& graph_pmem);
-	void benchmark_page_rank_sizes(const Benchmark::Parameters& tp, const GraphCRS<std::allocator>& graph_dram, const GraphCRS<PMEM::allocator>& graph_pmem);
-	void benchmark_breadth_first_traversal(const Benchmark::Parameters& tp, const GraphCRS<std::allocator>& graph_dram, const GraphCRS<PMEM::allocator>& graph_pmem);
-	void benchmark_memory(const Benchmark::Parameters& tp);
-	void benchmark_STREAM(const Benchmark::Parameters& tp);
+    void benchmark_page_rank(const Benchmark::Parameters& tp, const GraphCRS<std::allocator>& graph_dram, const GraphCRS<PMEM::allocator>& graph_pmem);
+    void benchmark_page_rank_sizes(const Benchmark::Parameters& tp, const GraphCRS<std::allocator>& graph_dram, const GraphCRS<PMEM::allocator>& graph_pmem);
+    void benchmark_breadth_first_traversal(const Benchmark::Parameters& tp, const GraphCRS<std::allocator>& graph_dram, const GraphCRS<PMEM::allocator>& graph_pmem);
+    void benchmark_memory(const Benchmark::Parameters& tp);
+    void benchmark_STREAM(const Benchmark::Parameters& tp);
 
-	std::vector<std::vector<double>> run_memory(const Benchmark::Parameters& tp, char* arr, size_t size);
+    std::vector<std::vector<double>> run_memory(const Benchmark::Parameters& tp, char* arr, size_t size);
 
-	template<template<class> class alloc_type, template<class> class temp_alloc_type>
-	std::vector<double> run_page_rank(const Benchmark::Parameters& tp, const GraphCRS<alloc_type>& graph) {
-		BlockTimer timer("Page Rank");
-		printf("Page Rank\n");
+    template<template<class> class alloc_type, template<class> class temp_alloc_type>
+    std::vector<double> run_page_rank(const Benchmark::Parameters& tp, const GraphCRS<alloc_type>& graph) {
+        BlockTimer timer("Page Rank");
+        printf("Page Rank\n");
 
-		std::vector<double> mteps_metrics;
+        std::vector<double> mteps_metrics;
 
-		printf("Number of Vertices: %lu = %s\n", graph.num_vertices(), FormatUtils::format_number(graph.num_vertices()).c_str());
-		printf("Number of Edges: %lu = %s\n", graph.num_edges(), FormatUtils::format_number(graph.num_edges()).c_str());
-		printf("Memory Size: %lu = %sB\n", graph.byte_size(), FormatUtils::format_number(graph.byte_size()).c_str());
-		printf("Function Templates: %s\n", BenchmarkUtils::parse_templates_from_signatures(__PRETTY_FUNCTION__).c_str());
-		printf("Number of Page Ranks: %u\n", tp.page_rank_num_dampening_factors);
-		printf("Number of Iterations: %u\n", tp.page_rank_iterations);
-		printf("Iteration, Time Elapsed (s), MTEPS\n");
-		for (uint32_t iter = 1; iter <= tp.test_iterations; iter++) {
-			Timer timer;
-			GraphAlgorithms::page_rank_v_neighbors<alloc_type, temp_alloc_type>(graph, tp.page_rank_iterations, std::vector<float>(tp.page_rank_num_dampening_factors, 0.8f));
-			timer.end();
+        printf("Number of Vertices: %lu = %s\n", graph.num_vertices(), FormatUtils::format_number(graph.num_vertices()).c_str());
+        printf("Number of Edges: %lu = %s\n", graph.num_edges(), FormatUtils::format_number(graph.num_edges()).c_str());
+        printf("Memory Size: %lu = %sB\n", graph.byte_size(), FormatUtils::format_number(graph.byte_size()).c_str());
+        printf("Function Templates: %s\n", BenchmarkUtils::parse_templates_from_signatures(__PRETTY_FUNCTION__).c_str());
+        printf("Number of Page Ranks: %u\n", tp.page_rank_num_dampening_factors);
+        printf("Number of Iterations: %u\n", tp.page_rank_iterations);
+        printf("Iteration, Time Elapsed (s), MTEPS\n");
+        for (uint32_t iter = 1; iter <= tp.test_iterations; iter++) {
+            Timer timer;
+            GraphAlgorithms::page_rank<alloc_type, temp_alloc_type>(graph, tp.page_rank_iterations, std::vector<float>(tp.page_rank_num_dampening_factors, 0.8f));
+            timer.end();
 
-			double time_elapsed_seconds = timer.get_time_elapsed() / 1e9;
-			mteps_metrics.push_back(((tp.page_rank_iterations * graph.num_edges()) / time_elapsed_seconds) / 1e6);
-			printf("%u, %.3f, %.3f\n", iter, time_elapsed_seconds, mteps_metrics.back());
-		}
+            double time_elapsed_seconds = timer.get_time_elapsed() / 1e9;
+            mteps_metrics.push_back(((tp.page_rank_iterations * graph.num_edges()) / time_elapsed_seconds) / 1e6);
+            printf("%u, %.3f, %.3f\n", iter, time_elapsed_seconds, mteps_metrics.back());
+        }
 
-		BenchmarkUtils::print_metrics("MTEPS", mteps_metrics);
+        BenchmarkUtils::print_metrics("MTEPS", mteps_metrics);
 
-		return mteps_metrics;
-	}
+        return mteps_metrics;
+    }
 
-	template<template<class> class alloc_type, template<class> class temp_alloc_type>
-	std::vector<double> run_breadth_first_traversal(const Benchmark::Parameters& tp, const GraphCRS<alloc_type>& graph, std::vector<uint32_t>& start_vertices) {
-		BlockTimer timer("Breadth First Traversal");
-		printf("Breadth First Traversal\n");
-		std::vector<double> mteps_metrics;
+    template<template<class> class alloc_type, template<class> class temp_alloc_type>
+    std::vector<double> run_breadth_first_traversal(const Benchmark::Parameters& tp, const GraphCRS<alloc_type>& graph, std::vector<uint32_t>& start_vertices) {
+        BlockTimer timer("Breadth First Traversal");
+        printf("Breadth First Traversal\n");
+        std::vector<double> mteps_metrics;
 
-		printf("Number of Vertices: %lu = %s\n", graph.num_vertices(), FormatUtils::format_number(graph.num_vertices()).c_str());
-		printf("Number of Edges: %lu = %s\n", graph.num_edges(), FormatUtils::format_number(graph.num_edges()).c_str());
-		printf("Memory Size: %lu = %sB\n", graph.byte_size(), FormatUtils::format_number(graph.byte_size()).c_str());
-		printf("Function Templates: %s\n", BenchmarkUtils::parse_templates_from_signatures(__PRETTY_FUNCTION__).c_str());
-		printf("Iteration, Time Elapsed (s), MTEPS\n");
-		for (uint32_t iter = 1; iter <= tp.test_iterations; iter++) {
-			Timer timer;
-			std::vector<int32_t, temp_alloc_type<int32_t>> vertex_depth = GraphAlgorithms::breadth_first_traversal_hybrid<alloc_type, temp_alloc_type>(graph, start_vertices[iter - 1]);
-			timer.end();
+        printf("Number of Vertices: %lu = %s\n", graph.num_vertices(), FormatUtils::format_number(graph.num_vertices()).c_str());
+        printf("Number of Edges: %lu = %s\n", graph.num_edges(), FormatUtils::format_number(graph.num_edges()).c_str());
+        printf("Memory Size: %lu = %sB\n", graph.byte_size(), FormatUtils::format_number(graph.byte_size()).c_str());
+        printf("Function Templates: %s\n", BenchmarkUtils::parse_templates_from_signatures(__PRETTY_FUNCTION__).c_str());
+        printf("Iteration, Time Elapsed (s), MTEPS\n");
+        for (uint32_t iter = 1; iter <= tp.test_iterations; iter++) {
+            Timer timer;
+            std::vector<int32_t, temp_alloc_type<int32_t>> vertex_depth = GraphAlgorithms::breadth_first_traversal_hybrid<alloc_type, temp_alloc_type>(graph, start_vertices[iter - 1]);
+            timer.end();
 
-			size_t num_vertices_checked = 0;
-			size_t num_edges_traversed = 0;
+            size_t num_vertices_checked = 0;
+            size_t num_edges_traversed = 0;
 
 #pragma omp parallel for schedule(static) reduction(+:num_vertices_checked) reduction(+:num_edges_traversed)
-			for (size_t vertex = 0; vertex < vertex_depth.size(); vertex++) {
-				if (vertex_depth[vertex] != -1) {
-					num_vertices_checked++;
-					num_edges_traversed += graph.num_neighbors(vertex);
-				}
-			}
+            for (size_t vertex = 0; vertex < vertex_depth.size(); vertex++) {
+                if (vertex_depth[vertex] != -1) {
+                    num_vertices_checked++;
+                    num_edges_traversed += graph.num_neighbors(vertex);
+                }
+            }
 
-			/*
-			*	If a graph is disconnected, we might have selected a source vertex
-			*	that is not part of the main graph. If we check the number of -1 vertex
-			*   depths, we can see how many vertices were actually checked. If its too
-			*	low, then we run again.
-			*/
-			if (num_vertices_checked < graph.num_vertices() * 0.25 || num_edges_traversed < graph.num_edges() * 0.25) {
-				start_vertices[iter - 1]++;
-				iter--;
-				continue;
-			}
+            /*
+            *	If a graph is disconnected, we might have selected a source vertex
+            *	that is not part of the main graph. If we check the number of -1 vertex
+            *   depths, we can see how many vertices were actually checked. If its too
+            *	low, then we run again.
+            */
+            if (num_vertices_checked < graph.num_vertices() * 0.25 || num_edges_traversed < graph.num_edges() * 0.25) {
+                start_vertices[iter - 1]++;
+                iter--;
+                continue;
+            }
 
-			double time_elapsed_seconds = timer.get_time_elapsed() / 1e9;
-			double edges_per_second = num_edges_traversed / time_elapsed_seconds;
+            double time_elapsed_seconds = timer.get_time_elapsed() / 1e9;
+            double edges_per_second = num_edges_traversed / time_elapsed_seconds;
 
-			mteps_metrics.push_back(edges_per_second / 1e6);
-			printf("%u, %.3f, %.3f\n", iter, time_elapsed_seconds, mteps_metrics.back());
-		}
+            mteps_metrics.push_back(edges_per_second / 1e6);
+            printf("%u, %.3f, %.3f\n", iter, time_elapsed_seconds, mteps_metrics.back());
+        }
 
-		BenchmarkUtils::print_metrics("MTEPS", mteps_metrics);
+        BenchmarkUtils::print_metrics("MTEPS", mteps_metrics);
 
-		return mteps_metrics;
-	}
+        return mteps_metrics;
+    }
 
 }
--- src/Main.cpp
+++ src/Main.cpp
@@ -12,74 +12,74 @@
 
 void print_info() {
 
-	printf("Performance Analytics of Graph Algorithms using Intel Optane DC Persistent Memory\n");
-	printf("by Evan Unmann\n");
+    printf("Performance Analytics of Graph Algorithms using Intel Optane DC Persistent Memory\n");
+    printf("by Evan Unmann\n");
 
-	printf("PMEM:\n");
-	printf("\tVersion: %d.%d\n", PMEM_MAJOR_VERSION, PMEM_MINOR_VERSION);
+    printf("PMEM:\n");
+    printf("\tVersion: %d.%d\n", PMEM_MAJOR_VERSION, PMEM_MINOR_VERSION);
 
-	/* Test the allocator to see if PMEM is accessible or not */
-	PMEM::allocator<float> allocator;
-	const size_t N = 10;
-	auto p = allocator.allocate(N);
-	printf("\tPMEM Accessible: %s\n", allocator.is_pmem() ? "True" : "False");
-	allocator.deallocate(p, N);
+    /* Test the allocator to see if PMEM is accessible or not */
+    PMEM::allocator<float> allocator;
+    const size_t N = 10;
+    auto p = allocator.allocate(N);
+    printf("\tPMEM Accessible: %s\n", allocator.is_pmem() ? "True" : "False");
+    allocator.deallocate(p, N);
 
-	printf("OpenMP:\n");
-	printf("\tNumber of Processors: %d\n", omp_get_num_procs());
-	printf("\tMaximum Threads: %d\n", omp_get_max_threads());
+    printf("OpenMP:\n");
+    printf("\tNumber of Processors: %d\n", omp_get_num_procs());
+    printf("\tMaximum Threads: %d\n", omp_get_max_threads());
 }
 
 int main(int argc, char** argv) {
-	BlockTimer timer("Time Elapsed");
+    BlockTimer timer("Time Elapsed");
 
-	try {
+    try {
 
-		print_info();
+        print_info();
 
-		Benchmark::Parameters tp = Benchmark::get_parameters();
+        Benchmark::Parameters tp = Benchmark::get_parameters();
 
-		std::vector<std::pair<std::string, std::string>> graph_paths;
-		graph_paths.push_back(std::make_pair("./graph_examples/facebook_combined.txt", "Facebook"));
-		graph_paths.push_back(std::make_pair("./graph_examples/soc-Epinions1.txt", "Epinions"));
-		graph_paths.push_back(std::make_pair("./graph_examples/soc-pokec-relationships.txt", "Pokec"));
-		graph_paths.push_back(std::make_pair("./graph_examples/sx-stackoverflow.txt", "Stack Overflow"));
-		graph_paths.push_back(std::make_pair("./graph_examples/soc-LiveJournal1.txt", "Live Journal"));
-		graph_paths.push_back(std::make_pair("./graph_examples/com-orkut.ungraph.txt", "Orkut"));
-		//graph_paths.push_back(std::make_pair("./graph_examples/com-friendster.ungraph.txt", "Friendster"));
+        std::vector<std::pair<std::string, std::string>> graph_paths;
+        graph_paths.push_back(std::make_pair("./graph_examples/facebook_combined.txt", "Facebook"));
+        graph_paths.push_back(std::make_pair("./graph_examples/soc-Epinions1.txt", "Epinions"));
+        graph_paths.push_back(std::make_pair("./graph_examples/soc-pokec-relationships.txt", "Pokec"));
+        graph_paths.push_back(std::make_pair("./graph_examples/sx-stackoverflow.txt", "Stack Overflow"));
+        graph_paths.push_back(std::make_pair("./graph_examples/soc-LiveJournal1.txt", "Live Journal"));
+        graph_paths.push_back(std::make_pair("./graph_examples/com-orkut.ungraph.txt", "Orkut"));
+        //graph_paths.push_back(std::make_pair("./graph_examples/com-friendster.ungraph.txt", "Friendster"));
 
-		for (const auto& graph_path : graph_paths) {
-			tp.graph_path = graph_path.first;
-			tp.graph_name = graph_path.second;
+        for (const auto& graph_path : graph_paths) {
+            tp.graph_path = graph_path.first;
+            tp.graph_name = graph_path.second;
 
-			GraphCRS<std::allocator> graph_dram;
-			GraphCRS<PMEM::allocator> graph_pmem;
+            GraphCRS<std::allocator> graph_dram;
+            GraphCRS<PMEM::allocator> graph_pmem;
 
-			printf("Loading %s graph from %s\n", tp.graph_name.c_str(), tp.graph_path.c_str());
-			{
-				BlockTimer timer("Graph DRAM Load");
-				graph_dram = GraphUtils::load<std::allocator>(tp.graph_path);
-			}
-			{
-				BlockTimer timer("Graph PMEM Copy");
-				graph_pmem = GraphCRS<PMEM::allocator>(graph_dram.num_vertices(), graph_dram.num_edges());
-				GraphUtils::copy(graph_dram, graph_pmem);
-			}
+            printf("Loading %s graph from %s\n", tp.graph_name.c_str(), tp.graph_path.c_str());
+            {
+                BlockTimer timer("Graph DRAM Load");
+                graph_dram = GraphUtils::load<std::allocator>(tp.graph_path);
+            }
+            {
+                BlockTimer timer("Graph PMEM Copy");
+                graph_pmem = GraphCRS<PMEM::allocator>(graph_dram.num_vertices(), graph_dram.num_edges());
+                GraphUtils::copy(graph_dram, graph_pmem);
+            }
 
-			Benchmark::benchmark_page_rank(tp, graph_dram, graph_pmem);
-			Benchmark::benchmark_page_rank_sizes(tp, graph_dram, graph_pmem);
-			Benchmark::benchmark_breadth_first_traversal(tp, graph_dram, graph_pmem);
+            //Benchmark::benchmark_page_rank(tp, graph_dram, graph_pmem);
+            Benchmark::benchmark_page_rank_sizes(tp, graph_dram, graph_pmem);
+            //Benchmark::benchmark_breadth_first_traversal(tp, graph_dram, graph_pmem);
 
-			graph_dram.free();
-			graph_pmem.free();
-		}
+            graph_dram.free();
+            graph_pmem.free();
+        }
 
-		//Benchmark::benchmark_memory(tp);
-		//Benchmark::benchmark_STREAM(tp);
-	}
-	catch (std::exception& e) {
-		printf("Exception: %s\n", e.what());
-	}
+        //Benchmark::benchmark_memory(tp);
+        //Benchmark::benchmark_STREAM(tp);
+    }
+    catch (std::exception& e) {
+        printf("Exception: %s\n", e.what());
+    }
 
-	return 0;
+    return 0;
 }
