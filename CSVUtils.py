f5970e9ae57db7a620d7c5524c7dceb3d24f8558
PerkGuacamole <evanunmann@gmail.com>
2021-04-28 09:59:03 -0400

    Adding ratios to mem metrics with script

--- CSVUtils.py
+++ CSVUtils.py
@@ -38,7 +38,28 @@ def transpose_pr(dir):
         pd.read_csv(csv_path, header=None).T.to_csv(csv_path[:-4] + "_T.csv", header=False, index=False)
 
 
-directory = "./output/2021.04.24.12.40.54"
+def add_ratio(dir):
+    csv_paths = glob.glob(path.join(directory, "mem_*.csv"))
 
-transpose_pr(directory)
-scale_pr(directory)
+    for csv_path in csv_paths:
+        df = pd.read_csv(csv_path)
+        dram_metrics = df["dram"]
+        pmem_metrics = df["pmem"]
+        ratio = []
+        for i in range(len(dram_metrics)):
+            dram_metric = dram_metrics[i]
+            pmem_metric = pmem_metrics[i]
+
+            d_v = float(dram_metric.strip().split(" ")[0])
+            p_v = float(pmem_metric.strip().split(" ")[0])
+
+            ratio.append("{:.2f}".format(p_v / d_v))
+
+        df["ratio"] = ratio
+
+        df.to_csv(csv_path[:-4] + "_R.csv")
+
+
+directory = "./results/"
+
+add_ratio(directory)
